1c9a1c2dcc6a9f91891f8e31b9e2f7e38eda700e
ashbounty <mwinter.1992@googlemail.com>
2018-03-19 19:04:24 +0100

    Interactive mode

--- 2_Law_of_Large_Numbers.py
+++ 2_Law_of_Large_Numbers.py
@@ -11,6 +11,7 @@ mpl.rcParams['grid.alpha'] = 0.2
 
 # Define a function that illustrates the law of large numbers
 def simulation(population, nmax, interactive=False):
+    if interactive==True: plt.ion()
     # Calculate population characteristics
     population_mean = np.mean(population)
     population_sd = np.var(population)**0.5
@@ -29,11 +30,18 @@ def simulation(population, nmax, interactive=False):
         sample.append(np.random.choice(population))
         # Recalculate the sample mean
         means.append(np.mean(sample))
-    plt.plot(means, color = 'darkblue', label = 'Sample averages')
-    plt.legend()
-    plt.show()
+        # Add to plot if interactive mode
+        if interactive==True: 
+            plt.scatter(n, np.mean(sample), s=1, color = 'darkblue')
+            plt.pause(10**-50)
+    if interactive==True:
+        plt.pause(0.5)
+    else:
+        plt.plot(means, color = 'darkblue', label = 'Sample averages')
+        plt.legend()
+        plt.show()
 
 # Tryin with different population
-simulation(np.random.normal(size=1000000), 2000)
-simulation(np.random.exponential(size=1000000), 2000)
-simulation(np.random.uniform(size=1000000), 2000)
+simulation(population = np.random.normal(size=1000000), nmax = 10000)
+simulation(population = np.random.exponential(size=1000000), nmax = 2000, interactive = True)
+simulation(population = np.random.uniform(size=1000000), nmax = 5000)
