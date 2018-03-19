9a9cb1ea6b5fced7fbf2345b9034bb8d16556590
ashbounty <mwinter.1992@googlemail.com>
2018-03-19 10:26:54 +0100

    Simulation function now more flexible

--- 2_Law_of_Large_Numbers.py
+++ 2_Law_of_Large_Numbers.py
@@ -10,25 +10,30 @@ mpl.rcParams['axes.grid'] = True
 mpl.rcParams['grid.alpha'] = 0.2
 
 # Define a function that illustrates the law of large numbers
-def simulation(nmax, mu, sigma, interactive=False):
+def simulation(population, nmax, interactive=False):
+    # Calculate population characteristics
+    population_mean = np.mean(population)
+    population_sd = np.var(population)**0.5
     # Initialize plot
     plt.title('Simulation: Convergence of sample mean')
     plt.xlabel('Sample Size')
     plt.ylabel('Sample Mean')
-    plt.axis([0, nmax, -1.65*sigma, 1.65*sigma])
-    plt.hlines(mu, 0, nmax, color = 'darkred', label = 'Theoretical mean')
+    plt.axis([0, nmax, population_mean-1.65*population_sd, population_mean+1.65*population_sd])
+    plt.hlines(population_mean, 0, nmax, color = 'darkred', label = 'Population mean')
     # Initialize lists
     sample = []
     means = []
     # Start of the simulation
     for n in range(1, nmax):
         # Append sample by iid new observation
-        sample.append(np.random.normal(mu, sigma))
+        sample.append(np.random.choice(population))
         # Recalculate the sample mean
         means.append(np.mean(sample))
     plt.plot(means, color = 'darkblue', label = 'Sample averages')
     plt.legend()
     plt.show()
 
-# Tryin
-simulation(10000, 0, 10)
+# Tryin with different population
+simulation(np.random.normal(size=1000000), 2000)
+simulation(np.random.exponential(size=1000000), 2000)
+simulation(np.random.uniform(size=1000000), 2000)
